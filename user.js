9b31716d00b662c5599448fae87e82b75d9b6577
Emmanuel <emmanuelkomba22@gmail.com>
2024-12-29 14:42:33 -0600

    Hitting an API

--- user.js
+++ user.js
@@ -1,4 +1,4 @@
-const postListEl = document.querySelector(".post-list");
+const postListEl = document.querySelector('.post-list')
 const id = localStorage.getItem("id");
 
 async function onSearchChange(event) {
@@ -8,20 +8,20 @@ async function onSearchChange(event) {
 
 async function renderPosts(id) {
     const posts = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`);
-    const postsData = await posts.json();
-    console.log(postsData)
-    
-    postListEl.innerHTML = postsData.map(post => postHTML(post)).join('')
+    const postsData = await posts.json(); 
+    postListEl.innerHTML = postsData.map((post) => postsHTML(post)).join('')
 }
 
-function postHTML(post) {
-    return `<div class="post">
-        <div class="post__title">
-            ${post.title}
-        </div>
-        <p class="post__body">
-            ${post.body}
-        </p>
+
+function postsHTML (post) {
+    return `
+    <div class="post">
+    <div class="post__title">
+    ${post.title}
+    </div>
+    <p class="post__body">
+    ${post.body}
+    </p>
     </div>`;
 }
 
